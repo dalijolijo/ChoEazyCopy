05fb20a5566abea9d5feebcb884e797ca073f47e
Dali <37987466+dalijolijo@users.noreply.github.com>
2020-05-29 23:59:16 +0200

    Rebranding

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,12 +9,12 @@ using ChoEazyCopy;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ChoEazyCopy")]
+[assembly: AssemblyTitle("EasyCopy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChoEazyCopy")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("EasyCopy")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,6 +53,6 @@ using ChoEazyCopy;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
 [assembly: ChoShellFileAssociation(AppHost.AppFileExt)]
